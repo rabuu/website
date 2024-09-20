69a52588266c4bc9b94f87807601877fce3e17e6
Rasmus Buurman <dev@rbuurman.de>
2024-09-20 17:21:24 +0200

    misc: add misc prototype site

--- deploy.sh
+++ deploy.sh
@@ -10,11 +10,18 @@ fi
 
 echo "Deploy to $DEPLOY_USER@rbuurman.de\n"
 
+echo
 echo 'common...'
 scp common/* $DEPLOY_USER@rbuurman.de:rbuurman.de/httpdocs
 
+echo
 echo 'home...'
 scp home/* $DEPLOY_USER@rbuurman.de:rbuurman.de/httpdocs
 
+echo
 echo 'dev...'
 scp dev/* $DEPLOY_USER@rbuurman.de:dev.rbuurman.de/httpdocs
+
+echo
+echo 'misc...'
+scp dev/* $DEPLOY_USER@rbuurman.de:misc.rbuurman.de/httpdocs
