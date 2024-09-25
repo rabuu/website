fbd905143172d11705d436dd0f7f41ee27625bb1
Rasmus Buurman <dev@rbuurman.de>
2024-09-25 22:14:21 +0200

    Make `content-block` a common element

--- common.js
+++ common.js
@@ -1,3 +1,5 @@
 import NavBar from "./modules/navbar.js";
+import ContentBlock from "./modules/contentblock.js";
 
 customElements.define("nav-bar", NavBar);
+customElements.define("content-block", ContentBlock);
---
+++ modules/contentblock.js
@@ -0,0 +1,50 @@
+const GHUSER = "rabuu";
+
+class ContentBlock extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    this.classList.add("content-block");
+    this.style.display = "block";
+
+    if (this.hasAttribute("desc")) {
+      let description = document.createElement("p");
+      description.classList.add("content-block-description");
+      description.innerHTML = this.getAttribute("desc");
+      this.prepend(description);
+    }
+
+    if (this.hasAttribute("title")) {
+      const title = this.getAttribute("title");
+
+      let headline = document.createElement("div");
+      headline.classList.add("content-block-headline");
+
+      let header = document.createElement("h2");
+      if (this.hasAttribute("href")) {
+        const href = this.getAttribute("href");
+        const gh = href === "github";
+
+        let img = document.createElement("img");
+        img.classList.add("content-block-link-img");
+        img.src = `/assets/${gh ? "github-mark.svg" : "link.png"}`;
+
+        headline.append(img);
+
+        let link = document.createElement("a");
+        link.href = gh ? `https://github.com/${GHUSER}/${title}` : href;
+        link.textContent = title;
+        header.append(link);
+      } else {
+        header.textContent = title;
+      }
+
+      headline.append(header);
+      this.prepend(headline);
+    }
+  }
+}
+
+export default ContentBlock;
