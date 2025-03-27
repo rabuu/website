0cf6489aaac25849864486e5bb6fe717a37d416d
Rasmus Buurman <rasmus@rbuurman.de>
2025-03-27 01:43:40 +0100

    Use pass in deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,15 @@
 #!/usr/bin/env sh
 
-set -e
+set -xe
 
-if [ -z "$DEPLOY_USER" ]; then
-    echo 'Variable $DEPLOY_USER not set'
-    exit 1
-fi
+user=$(pass netcup.de/hosting/8000/username)
+password=$(pass netcup.de/hosting/8000/password)
 
+echo "Deploy to $user@rbuurman.de\n"
 
-echo "Deploy to $DEPLOY_USER@rbuurman.de\n"
-
-scp -r \
+sshpass -p "$password" scp -r \
     common.js common.css \
     index.html style.css favicon.ico \
     assets modules \
     projekte krimskrams \
-    "$DEPLOY_USER@rbuurman.de:rbuurman.de/httpdocs"
+    "$user@rbuurman.de:rbuurman.de/httpdocs"
