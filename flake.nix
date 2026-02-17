ea11a75c74da1b6c345b443dfb860bb229cbe2e6
Rasmus Buurman <rasmus@rbuurman.de>
2026-02-17 17:52:29 +0100

    Add simple nix devshell

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "My website";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          name = "website";
+
+          packages = with pkgs; [
+          ];
+        };
+      }
+    );
+}
